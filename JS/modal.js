2f6299122a59a7bb238f25a862bd14f01d165d5d
Алексей Андрияненко <lex_flame@mail.ua>
2018-12-03 00:58:44 +0200

    Сделал серверную обработку.

--- JS/modal.js
+++
@@ -1,7 +0,0 @@
-
-  document.querySelector('.button-callback').addEventListener('click', function(){
-    document.querySelector('.modal').classList.toggle('modal-visible');
-  });
-  document.querySelector('.modal').addEventListener('click', function(){
-    document.querySelector('.modal').classList.toggle('modal-visible')
-  })
---
+++ mail.php
@@ -0,0 +1,7 @@
+<?php 
+
+mail(
+  "lex_flame@mail.ua"
+  "Ну, прувет"
+  $_REQUEST["user_name"] . " " . $_REQUEST["user_Phone"]
+);
